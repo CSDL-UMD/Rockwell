5d7b9abea2f7e3f5dfd175c774b78c6ccb8efffa
saumyabhadani95 <saumyabhadani95@gmail.com>
2021-01-21 11:37:06 -0500

    Removed Tweepy

--- twitter_api/engagement_servers/Retweet.py
+++ twitter_api/engagement_servers/Retweet.py
@@ -1,5 +1,6 @@
 """ Read the credentials from credentials.txt and place them into the `cred` dictionary """
-import tweepy
+#import tweepy
+from requests_oauthlib import OAuth1Session
 import json
 from flask import Flask, render_template, request, url_for, jsonify
 
@@ -16,14 +17,20 @@ def retweet():
         cred[name] = value.strip()
     f.close()
 
-    auth = tweepy.OAuthHandler(cred["key"], cred["key_secret"])
-    auth.set_access_token(cred["token"], cred["token_secret"])
-    api = tweepy.API(auth)
+    #auth = tweepy.OAuthHandler(cred["key"], cred["key_secret"])
+    #auth.set_access_token(cred["token"], cred["token_secret"])
+    #api = tweepy.API(auth)
+    oauth = OAuth1Session(cred['key'],
+                       client_secret=cred['key_secret'],
+                       resource_owner_key=cred['token'],
+                       resource_owner_secret=cred['token_secret'])
 
     data = request.get_json()
     tweet_id = data['tweet_id']
     try:
-        api.retweet(int(tweet_id))
+        #api.retweet(int(tweet_id))
+        #params = {"id": int(tweet_id)}
+        response_retweet = oauth.post("https://api.twitter.com/1.1/statuses/retweet/"+tweet_id+".json")
         return jsonify({"success":1}) # Retweet successful
     except Exception as e:
         print(e)
@@ -38,15 +45,21 @@ def like():
         cred[name] = value.strip()
     f.close()
 
-    auth = tweepy.OAuthHandler(cred["key"], cred["key_secret"])
-    auth.set_access_token(cred["token"], cred["token_secret"])
-    api = tweepy.API(auth)
+    #auth = tweepy.OAuthHandler(cred["key"], cred["key_secret"])
+    #auth.set_access_token(cred["token"], cred["token_secret"])
+    #api = tweepy.API(auth)
+    oauth = OAuth1Session(cred['key'],
+                       client_secret=cred['key_secret'],
+                       resource_owner_key=cred['token'],
+                       resource_owner_secret=cred['token_secret'])
 
     data = request.get_json()
     tweet_id = data['tweet_id']
     try:
-        tweet = api.get_status(int(tweet_id))
-        tweet.favorite()
+        #tweet = api.get_status(int(tweet_id))
+        #tweet.favorite()
+        response_like = oauth.post("https://api.twitter.com/1.1/favorites/create.json",params = {"id":int(tweet_id)})
+        print(response_like)
         return jsonify({"success":1}) # Retweet successful
     except Exception as e:
         print(e)
