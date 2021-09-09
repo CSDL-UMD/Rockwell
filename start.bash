dd73c6b8da0ee5b979337ed96254b729d644110e
Rob <randion@usf.edu>
2021-09-09 19:11:46 -0400

    Shell file update

--- start.bash
+++ start.bash
@@ -1,23 +1,23 @@
 #!/bin/bash
-command="$0"
+command=$0
 start="start"
 stop="stop"
 restart="restart"
-if [$command == $start]
+if [$command = $start]
 then
     tmux new-session -s NodeApp -d 'node ./app.js'
     tmux new-session -s Authorizer -d 'cd ./twitter_api/twauth-web-master/ && python3 ./twauth-web.py'
     tmux new-session -s FeedRendering -d 'cd ./twitter_api/guest_access_tweepy/ && python3 ./guest_access_twitter_flask.py'
     tmux new-session -s DatabaseAccess -d 'cd ./twitter_api/database_access/ && python3 ./database_access.py'
     tmux new-session -s Engagement -d 'cd ./twitter_api/engagement_servers/ && python3 ./Retweet.py'
-elif [$command == $stop]
+elif [$command = $stop]
 then
     tmux kill-session -t NodeApp
     tmux kill-session -t Authorizer
     tmux kill-session -t FeedRendering
     tmux kill-session -t DatabaseAccess
     tmux kill-session -t Engagement
-elif [$command == $restart]
+elif [$command = $restart]
 then
     tmux kill-session -t NodeApp
     tmux kill-session -t Authorizer
