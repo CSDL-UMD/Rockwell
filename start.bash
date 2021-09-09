a3fb28e551304586217c05f965b1b7bf9c8b9f13
Robert <randion@usf.edu>
2021-09-09 15:36:26 -0400

    Work in progress to script

--- start.bash
+++ start.bash
@@ -1,19 +1,23 @@
 #!/bin/bash
-if [$0 == 'start']
+command="$0"
+start="start"
+stop="stop"
+restart="restart"
+if [$command == $start]
 then
     tmux new-session -s NodeApp -d 'node ./app.js'
     tmux new-session -s Authorizer -d 'cd ./twitter_api/twauth-web-master/ && python3 ./twauth-web.py'
     tmux new-session -s FeedRendering -d 'cd ./twitter_api/guest_access_tweepy/ && python3 ./guest_access_twitter_flask.py'
     tmux new-session -s DatabaseAccess -d 'cd ./twitter_api/database_access/ && python3 ./database_access.py'
     tmux new-session -s Engagement -d 'cd ./twitter_api/engagement_servers/ && python3 ./Retweet.py'
-elif [$0 == 'stop']
+elif [$command == $stop]
 then
     tmux kill-session -t NodeApp
     tmux kill-session -t Authorizer
     tmux kill-session -t FeedRendering
     tmux kill-session -t DatabaseAccess
     tmux kill-session -t Engagement
-elif [$0 == 'restart']
+elif [$command == $restart]
 then
     tmux kill-session -t NodeApp
     tmux kill-session -t Authorizer
@@ -28,3 +32,4 @@ then
     tmux new-session -s Engagement -d 'cd ./twitter_api/engagement_servers/ && python3 ./Retweet.py'
 else
     echo "No valid argument given, start,stop,restart."
+fi
