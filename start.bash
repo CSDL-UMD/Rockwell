263d4b31d3546b3c150a76d2c0785d85ae44da83
Robert <randion@usf.edu>
2021-09-10 19:10:00 -0400

    Working shell file with 3 modes

--- start.bash
+++ start.bash
@@ -1,23 +1,20 @@
 #!/bin/bash
-command=$0
-start="start"
-stop="stop"
-restart="restart"
-if [$command = $start]
+
+if (($1 == 0))
 then
     tmux new-session -s NodeApp -d 'node ./app.js'
     tmux new-session -s Authorizer -d 'cd ./twitter_api/twauth-web-master/ && python3 ./twauth-web.py'
     tmux new-session -s FeedRendering -d 'cd ./twitter_api/guest_access_tweepy/ && python3 ./guest_access_twitter_flask.py'
     tmux new-session -s DatabaseAccess -d 'cd ./twitter_api/database_access/ && python3 ./database_access.py'
     tmux new-session -s Engagement -d 'cd ./twitter_api/engagement_servers/ && python3 ./Retweet.py'
-elif [$command = $stop]
+elif (($1 == 1))
 then
     tmux kill-session -t NodeApp
     tmux kill-session -t Authorizer
     tmux kill-session -t FeedRendering
     tmux kill-session -t DatabaseAccess
     tmux kill-session -t Engagement
-elif [$command = $restart]
+elif (($1 == 2))
 then
     tmux kill-session -t NodeApp
     tmux kill-session -t Authorizer
@@ -31,5 +28,5 @@ then
     tmux new-session -s DatabaseAccess -d 'cd ./twitter_api/database_access/ && python3 ./database_access.py'
     tmux new-session -s Engagement -d 'cd ./twitter_api/engagement_servers/ && python3 ./Retweet.py'
 else
-    echo "No valid argument given, start,stop,restart."
+    echo "Enter 0 for start, 1 for stop, and 2 for restart as a command line argument."
 fi
