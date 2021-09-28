1579f6052fb5c8b87c2a8d412790bfdd6c5dea7c
Robert <randion@usf.edu>
2021-09-28 10:34:29 -0400

    Carousel Component

--- backend/README.md
+++ backend/README.md
@@ -1,2 +1,3 @@
 Python backend of Rockwell
+Run develope from: ``` python3 setup.py develop ```
 Install from setup.py: ``` pip install -e . ```
---
+++ frontend/src/components/Carousel/Carousel.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Carousel from 'react-bootstrap/Carousel'
+
+function Carousel(){
+
+    return (
+        <div>
+            I work
+        </div>
+    )
+}
+
+export default Carousel;
