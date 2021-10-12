aeb3c6022384d2065160e54fb1aa76851eb3d53a
Thomas Bivins <bivinsthomas@gmail.com>
2021-10-12 10:14:46 -0400

    Carousel implementation

--- frontend/src/components/Carousel/CarouselComponent.js
+++ frontend/src/components/Carousel/CarouselComponent.js
@@ -1,14 +1,63 @@
-import React from 'react';
-// import Carousel from 'react-bootstrap/Carousel'; Bootstrap import
-// import './Carousel.css';
+import React, { useState } from 'react';
+import { CarouselItem } from 'react-bootstrap';
+import Carousel from 'react-bootstrap/Carousel';
+import {SliderData} from './SliderData';
+import BtnSlider from './BtnSlider';
+import './Carousel.css';
 
-function CarouselComponent(){
+//const {current, setCurrent} = useState{0}
+//const length = slides.length
+function CarouselComponent() {
+  
+  const [slideIndex, setSlideIndex] = useState(1)
+  const nextSlide = () => {
+    if(slideIndex !== SliderData.length){
+      setSlideIndex(slideIndex + 1)
+    }
+    else if(slideIndex === SliderData.length){
+      setSlideIndex(1)
+    }
+  }
+  const prevSlide = () => {
+    if(slideIndex !== 1){
+      setSlideIndex(slideIndex - 1)
+    }
+    else if(slideIndex === 1){
+      setSlideIndex(SliderData.length)
+    }
+  }
 
-    return (
-        <div>
-            I work right
-        </div>
-    )
+  const moveDot = index => {
+    setSlideIndex(index)
+  }
+
+  return (
+    <div className="slider-container">
+      {SliderData.map((slide, i) => {
+        return (
+          <div className = {slideIndex === i + 1 ? "slide active-anim" : "slide"}>
+            <img
+              src={`/Instruction_Images/Screenshot${i + 1}.png`}
+              alt="" />
+          </div>
+        )
+      })}
+      <BtnSlider moveSlide={nextSlide} direction = {"next"}/>
+      <BtnSlider moveSlide={prevSlide} direction = {"prev"}/>
+      {/*I work right*/}
+
+      <div className = "container-dots">
+        {Array.from({length: SliderData.length}).map((item, index) => (
+          <div onClick = {() => moveDot(index + 1)}
+          className={slideIndex === index + 1 ? "dot active" : "dot"}></div>
+        ))}
+      </div>
+    </div>
+  )
 }
 
 export default CarouselComponent;
+/*SliderData[index + 1].image*/
+
+/* src={slide.image} key={i}
+alt="" /> */
---
+++ frontend/src/components/Carousel/SliderData.js
@@ -0,0 +1,87 @@
+export const SliderData = [
+  {
+      image: './Instruction_Images/Screenshot1.png'
+  },
+  {
+      image: './Instruction_Images/Screenshot2.png'
+  },
+  {
+      image: './Instruction_Images/Screenshot3.png'
+  },
+  {
+      image: './Instruction_Images/Screenshot4.png'
+  },
+  {
+      image: './Instruction_Images/Screenshot5.png'
+  },
+  {
+      image: './Instruction_Images/Screenshot6.png'
+  },
+  {
+      image: './Instruction_Images/Screenshot7.png'
+  },
+  {
+      image: './Instruction_Images/Screenshot8.png'
+  },
+  {
+      image: './Instruction_Images/Screenshot9.png'
+  },
+]
+
+//export default SliderData
+
+
+
+/*
+import { v4 as uuidv4 } from "uuid";
+
+const SliderData = [
+  {
+    id: uuidv4(),
+    title: "Lorem ipsum",
+    subTitle: "Lorem"
+  },
+  {
+    id: uuidv4(),
+    title: "Lorem ipsum",
+    subTitle: "Lorem"
+  },
+  {
+    id: uuidv4(),
+    title: "Lorem ipsum",
+    subTitle: "Lorem"
+  },
+  {
+    id: uuidv4(),
+    title: "Lorem ipsum",
+    subTitle: "Lorem"
+  },
+  {
+    id: uuidv4(),
+    title: "Lorem ipsum",
+    subTitle: "Lorem"
+  },
+  {
+    id: uuidv4(),
+    title: "Lorem ipsum",
+    subTitle: "Lorem"
+  },
+  {
+    id: uuidv4(),
+    title: "Lorem ipsum",
+    subTitle: "Lorem"
+  },
+  {
+    id: uuidv4(),
+    title: "Lorem ipsum",
+    subTitle: "Lorem"
+  },
+  {
+    id: uuidv4(),
+    title: "Lorem ipsum",
+    subTitle: "Lorem"
+  },
+];
+
+export default SliderData;
+*/
