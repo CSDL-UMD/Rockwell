b413a6e0ef76a670ee4d1b0bcbfa6dea424b7555
Robert Andion <randion@usf.edu>
2022-01-10 15:01:36 -0500

    Addition of a landing page, automatic authorizer redirect

--- frontend/src/Configuration/config.js
+++ frontend/src/Configuration/config.js
@@ -1,7 +1,8 @@
 const config = {
   get_feed: 'http://127.0.0.1:5051/getfeed',
   like_tweet: 'http://127.0.0.1:5050/like',
-  retweet_tweet: 'http://127.0.0.1:5050/retweet'
+  retweet_tweet: 'http://127.0.0.1:5050/retweet',
+  authorizer: 'http://127.0.0.1:5000'
 }
 
 export default config;
