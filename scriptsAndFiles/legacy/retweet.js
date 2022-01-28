ace6461a245ec8511c000ec716e5566dbae4845b
Robert Andion <randion@usf.edu>
2022-01-28 11:33:34 -0500

    Removal of unneeded legacy code

--- scriptsAndFiles/legacy/retweet.js
+++
@@ -1,7 +0,0 @@
-function retweet_clicked(tweet_id) {
-  console.log('In Retweet JS')
-  //var spawn = require("child_process").spawn;
-  const { spawn } = require('child_process');
-  var process = spawn('python',["./Retweet.py", 
-                            tweet_id] );    
-}
