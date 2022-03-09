d61d5e94c4a6be74d9d22a54ef081e27df3559ae
Robert Andion <randion@usf.edu>
2022-03-09 10:10:26 -0500

    Working solution with 2 file writes

--- backend/src/eligibility/FileIO/WriteOut.js
+++ backend/src/eligibility/FileIO/WriteOut.js
@@ -1,14 +1,37 @@
 const fs = require('fs');
 var path = require('path');
+const zlib = require('zlib');
 
 const writeOut = async (writeObject, twitterId) => {
-    let jsonPath = path.join(__dirname, '..', 'User_Data', twitterId + '.json');
-    fs.writeFile(jsonPath,JSON.stringify(writeObject, null, 4), function (err,data) {
-        if (err) {
-          return console.log(err);
-        }
-        console.log(data);
+  let jsonPath = path.join(__dirname, '..', 'User_Data', twitterId + '.json');
+  fs.writeFile(jsonPath, JSON.stringify(writeObject, null, 4), function (err, data) {
+    if (err) {
+      return console.log(err);
+    }
+    const fileContents = fs.createReadStream(jsonPath);
+    const zip = zlib.createGzip();
+    let jsonPathZip = path.join(__dirname, '..', 'User_Data', twitterId + '.json.gz');
+    const writeStream = fs.createWriteStream(jsonPathZip);
+
+    fileContents.pipe(zip).pipe(writeStream).on('finish', (err) => {
+      if (err)
+        return console.log(err);
+      console.log("orlo");
+      fileContents.destroy();
+      writeStream.destroy();
+      deleteFile(jsonPath);
+    });
+
+  });
+
+  const deleteFile = (fileName) => {
+    // Try to delete unzipped file
+      console.log("hi");
+      fs.unlink(fileName, (err) => {
+        if (err) return console.log(err);
       });
+  };
+
 };
 
 module.exports = writeOut;
