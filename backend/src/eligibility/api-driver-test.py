4dfcac4b863dcb99437d1f259a3d8335f7290170
Giovanni Luca Ciampaglia <glciampagl@gmail.com>
2022-05-13 21:55:14 -0400

    Make three separate requests in API driver script

--- backend/src/eligibility/api-driver-test.py
+++ backend/src/eligibility/api-driver-test.py
@@ -5,22 +5,46 @@ import os
 def current_milli_time():
     return round(time.time() * 1000)
 
+endpoints = ["hometimeline", "usertimeline", "favorites"]
+
+host = "127.0.0.1"
+port = 6000
+
+# placeholder values
+mturk_id = 123
+mturk_hit_id = 456
+mturk_assignment_id = 789
+
+# get access token and access token secret from environment
 try:
-    KEY = os.environ['KEY']
-    KEY_SECRET = os.environ['KEY_SECRET']
+    access_token = os.environ['KEY']
+    access_token_secret = os.environ['KEY_SECRET']
 except KeyError as e:
     print(f"Error: no {e.args[0]} found in the environment. Please export it.")
     import sys
     sys.exit(1)
 
-# eligibility/:access_token&:access_token_secret&:mturk_id&:mturk_hit_id&:mturk_assignment_id
+# URL format is:
+#
+# /api/:endpoint/:access_token&:access_token_secret&:mturk_id&:mturk_hit_id&:mturk_assignment_id
 
-start = current_milli_time()
-res = rq.get("http://127.0.0.1:6000/eligibility/" + KEY + '&' + KEY_SECRET + "&123&456&789")
-end = current_milli_time()
-out = res.json()
-if not out['error']:
-    print(out)
-    print("Elapsed time: " + str(end - start))
-else:
-    print("API Rate-Limit reached")
+total_time = 0
+for endpoint in endpoints:
+    url_path = f"/api/{endpoint}/{access_token}&{access_token_secret}" \
+        "&{mturk_id}&{mturk_hit_id}&{mturk_assignment_id}"
+    print(f"Querying {endpoint}...")
+    start = current_milli_time()
+    res = rq.get(f"http://{host}:{port}{url_path}")
+    end = current_milli_time()
+    if res.ok:
+        out = res.json()
+        if not out['error']:
+            print(out)
+            print("Elapsed time: " + str(end - start))
+        else:
+            print("API Rate-Limit reached")
+    else:
+        print(f"Error: response failed with status: {res.status_code}")
+    print()
+    total_time += (end - start)
+print(f"Total elapsed time: {total_time}")
