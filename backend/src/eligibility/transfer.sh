e6b3c9fc2d9195418ff1fccac03762ad48994bc1
Giovanni Luca Ciampaglia <glciampagl@gmail.com>
2023-01-09 13:50:20 -0500

    Added transfer script

---
+++ backend/src/eligibility/transfer.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+set -euo pipefail
+
+if [[ ${#} != 2 ]];
+then
+    >&2 echo "Error: wrong number of arguments: ${*}"
+    >&2 echo "Usage: ${0} DIR DEST"
+    >&2 echo "  DIR - local directory"
+    >&2 echo "  DEST - s3 bucket"
+    exit 1
+fi
+DIR=${1}
+DEST=${2}
+MINUTES=$((60*24*3))
+pushd ${DIR} 1>/dev/null
+FILES=($(find . -type f -cmin +${MINUTES} -and -iname \*.json.gz))
+for file in ${FILES[@]};
+do
+    aws s3 mv ${file} ${DEST}
+done
+popd 1>/dev/null
