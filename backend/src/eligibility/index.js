cc4f5b2f615baab24a91ff6cb41f0fb038cede6f
Ubuntu <ubuntu@ip-10-0-1-196.us-east-2.compute.internal>
2023-01-13 20:41:46 +0000

    Use port 6000 instead of 5000

--- backend/src/eligibility/index.js
+++ backend/src/eligibility/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const cors = require('cors');
 const userRoutes = require('./routes/Users');
-const port = 5000;
+const port = 6000;
 
 app.use(express.json());
 app.use(cors());
