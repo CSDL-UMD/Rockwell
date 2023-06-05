b5c29087d2a038b7e6e69cf7bf8d34fbd02cc73e
Saumya Bhadani <saumya@pop-os.localdomain>
2023-06-04 23:07:17 -0400

    Added immediate pushing to DB feature

--- backend/src/authorizer/twauth-web-v2.py
+++ backend/src/authorizer/twauth-web-v2.py
@@ -30,7 +30,6 @@ def config(filename='database.ini', section='postgresql'):
     parser = ConfigParser()
     # read config file
     parser.read(filename)
-yo yo honey singh
     # get section, default to postgresql
     db = {}
     if parser.has_section(section):
